932f93bba3c60fe1a946c09788f80639121be5b6
dbe <davidbrianethier@gmail.com>
2019-01-28 09:16:48 -0700

    Full canvas of circles

--- index.js
+++ index.js
@@ -1,5 +1,60 @@
-checkSVG();
+const ROWS = 13;
+const COLS = 18;
 
+const EVEN_OFFSET = 81
+const ODD_OFFSET = 144
+
+const DIST = 126
+
+let canvas;
+
+function draw() {
+  canvas = SVG('drawing').size(2250.76, 1404.72)
+
+  for(let row = 0; row < ROWS; row++) {
+    let cols = (row % 2 == 0) ? COLS : COLS - 1
+    for(let col = 0; col < cols; col++) {
+      let circle = drawCircle();
+
+      if(row % 2 == 0) {
+        circle.cx(EVEN_OFFSET + (col * DIST)).cy(81 + (DIST * row))
+      } else {
+        circle.cx(ODD_OFFSET + (col * DIST)).cy(81 + (DIST * row))
+      }
+    }
+  }
+
+  console.log(canvas.svg())
+}
+
+
+function drawCircle() {
+  return canvas.circle(100).attr({
+    fill: '#fff',
+    stroke: '#000'
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+checkSVG();
 function checkSVG() {
   if(window.SVG === undefined) {
     console.log("Undefined, checking again")
@@ -8,11 +63,4 @@ function checkSVG() {
     console.log("SVG: ", SVG);
     draw();
   }
-}
-
-function draw() {
-  var draw = SVG('drawing').size(300, 300)
-  var rect = draw.rect(200, 200).attr({ fill: '#f06' })
-
-  console.log(rect.svg())
 }
